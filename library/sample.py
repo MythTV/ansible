853500f4a22d858a665ce7537fad9f0ff178523b
Bill Meek <bmeek@mythtv.org>
2025-12-19 14:52:22 -0600

    modules/filters: add directories to complete change

---
+++ library/sample.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+#!/home/bill/.virtualenvs/mythtv/bin/python
+
+#### This is coppied from another module and would need to be re-written
+#### to to whatever is needed. Additional files can be added just like
+#### this with other module names.
+
+''' Overall comment here '''
+
+# Copyright: (c) 2025,  Bill Meek based on Ansible documentation
+# GNU General Public License v3.0+ (see COPYING or
+# https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# pylint: disable=invalid-name
+__metaclass__ = type
+# pylint: enable=invalid-name
+
+from ansible.module_utils.basic import AnsibleModule  # nopep8
+
+
+def run_module():
+    ''' Update with real module information. '''
+
+    module_args = {
+        "api_port": {"type": 'int', "required": False, "default": 6544},
+        "host": {"type": 'str', "required": False, "default": 'localhost'},
+        }
+
+    result = {'changed': False}
+
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
+
+    # In the check mode, really a sanity test of this module.
+    if module.check_mode:
+        result['changed'] = True
+        module.exit_json(**result)
+
+    # Do something(s) here and resurn result information.
+
+    result['comment'] = 'library/sample.py ran'
+    result['host'] = module.params['host']
+    result['port'] = module.params['api_port']
+
+    module.exit_json(**result)
+
+
+def main():
+    ''' Common for all modules '''
+
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
